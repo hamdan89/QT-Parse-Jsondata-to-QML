0db0bbdc82ae3348abd45487ec710c7ccb4b87cb
Hamdan Hejazi <eng.hamdano@hotmail.com>
2017-11-25 15:26:02 +0100

    create NetworkManager header and class

---
+++ networkmanager.h
@@ -0,0 +1,12 @@
+#ifndef NETWORKMANAGER_H
+#define NETWORKMANAGER_H
+
+#include <QObject>
+
+class NetworkManager : public NetworkManager
+{
+public:
+    NetworkManager();
+};
+
+#endif // NETWORKMANAGER_H
